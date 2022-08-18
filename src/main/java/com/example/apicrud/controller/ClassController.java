92ab7f63dfd43cd4889bb6f050d9555aae4538dc
Viet Hoang <viethoangd33@gmail.com>
2022-08-18 19:39:35 +0700

    Initial commit

---
+++ src/main/java/com/example/apicrud/controller/ClassController.java
@@ -0,0 +1,77 @@
+package com.example.apicrud.controller;
+
+import com.example.apicrud.service.ClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/class")
+public class ClassController {
+    @Autowired
+    ClassService classService;
+
+    @RequestMapping(value ="list",method = RequestMethod.GET)
+    public ResponseEntity<List<Class>> findAllClass(){
+        List<Class> lsClass = classService.findAll();
+        if(lsClass.size()==0){
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new  ResponseEntity<List<Class>>(lsClass,HttpStatus.OK);
+    }
+
+
+
+    //userbyname?name=quang
+    @RequestMapping(value ="classbyname",method = RequestMethod.GET)
+    public ResponseEntity<List<Class>> findAllClass(@PathParam("name") String name){
+        List<Class> lsClass = classService.findAllByName(name);
+        if(lsClass.size()==0){
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new  ResponseEntity<List<Class>>(lsClass,HttpStatus.OK);
+    }
+
+
+
+
+    @RequestMapping(value ="create",method = RequestMethod.POST)
+    public ResponseEntity<Class> saveNewClass(@RequestBody Class c ){
+        classService.saveClass(c);
+        return new ResponseEntity<Class>(c,HttpStatus.OK);
+    }
+    //    http://localhost:8080/updateUser
+    @RequestMapping(value ="update",method = RequestMethod.PUT)
+    public ResponseEntity<Class> saveNewClass(
+            @Param("id") Integer id,
+            @RequestBody Class c ){
+        Class oldClass = classService.findById(id);
+        oldClass.setName(c.getName());
+        oldClass.setRoom(c.getRoom());
+        oldClass.setNote(c.getNote());
+        classService.saveClass(oldClass);
+        return new ResponseEntity<Class>(oldClass,HttpStatus.OK);
+    }
+//    @RequestMapping(value ="updateUser1/{id}",method = RequestMethod.PUT) // "updateUser1/{id}"
+//    public ResponseEntity<User> saveNewUser(
+//            @PathVariable(value = "id") Integer id, //@PathVariable(value = "id")
+//            @RequestBody User user ){
+//        User oldUser = userService.findById(id);
+//        oldUser.setName(user.getName());
+//        oldUser.setEmail(user.getEmail());
+//        oldUser.setPhone(user.getPhone());
+//        userService.saveUser(oldUser);
+//        return new ResponseEntity<User>(oldUser,HttpStatus.OK);
+//    }
+
+    @RequestMapping(value ="delete/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Class> deleteClass(@PathVariable(value = "id") Integer id){
+        classService.deleteClass(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/example/apicrud/service/UserServiceimpl.java
@@ -0,0 +1,40 @@
+package com.example.apicrud.service;
+
+
+import com.example.apicrud.reponstory.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceimpl implements UserService {
+    @Autowired
+
+    UserRepository userRepository;
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findById(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public List<User> findAllByName(String name){
+        return userRepository.findAllByName(name);
+    }
+}
